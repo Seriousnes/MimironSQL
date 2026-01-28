83d13955e97b65541dbf1685b925a33427536fb6
Sean Smith <ranky26@gmail.com>
2026-01-29 09:21:58 +1000

    Remove legacy

--- Legacy/Db2/Wdc5/FieldMetaData.cs
+++
@@ -1,3 +0,0 @@
-namespace MimironSQL.Db2.Wdc5;
-
-public readonly record struct FieldMetaData(short Bits, short Offset);
--- Legacy/Db2/Wdc5/SparseEntry.cs
+++
@@ -1,6 +0,0 @@
-using System.Runtime.InteropServices;
-
-namespace MimironSQL.Db2.Wdc5;
-
-[StructLayout(LayoutKind.Sequential, Pack = 2)]
-public readonly record struct SparseEntry(uint Offset, ushort Size);
